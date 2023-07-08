13b993f17b0f29d79655ed1ba855e7020ea397b8
NikolozFilishvili <pilishvilinika@gmail.com>
2023-07-08 20:52:04 +0400

    .commit

--- p1/p/something.py
+++ p1/p/something.py
@@ -1,1 +1,4 @@
-print("hello world")
+print("Naming your Band")
+City = input("where are your roots?\n")
+penname = input("what is your pen name\n")
+print("your Band name could be " + "" + City + " " + penname)
