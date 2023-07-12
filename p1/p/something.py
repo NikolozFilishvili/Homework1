720add0c0ab209b8c9ca7692e29d73df614aaa80
NikolozFilishvili <pilishvilinika@gmail.com>
2023-07-12 20:05:50 +0400

    .i

--- p1/p/something.py
+++ p1/p/something.py
@@ -1,4 +1,5 @@
 print("Naming your Band")
-City = input("where are your roots?\n")
-penname = input("what is your pen name\n")
-print("your Band name could be " + "" + City + " " + penname)
+City = input("where are your roots?")
+Nickname = input("what is your Nickname?")
+print("your Band name can be " + "" + City + " " + Nickname)
+
