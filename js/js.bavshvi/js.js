f05059fa3c0ec93dd6a5b29efce86ca01d200a60
NikolozFilishvili <pilishvilinika@gmail.com>
2023-08-24 22:18:29 +0400

    .adas

--- js/js.bavshvi/js.js
+++ js/js.bavshvi/js.js
@@ -29,39 +29,69 @@
 // document.write(
 //   newbalghi.height.length + newbagana.height.length + newbavshvi.height.length
 // );
-es_araswori_rom_iyo_pirveli_xolo_meore_sworia = truth
-function Bavshvi(rizz, height, weight, aerodynamics){
+// es_araswori_rom_iyo_pirveli_xolo_meore_sworia = truth
+// function Bavshvi(rizz, height, weight, aerodynamics){
+//     this.rizz = rizz;
+//     this.height = height;
+//     this.weight = weight;
+//     this.aerodynamics = aerodynamics;
+//     this.printer = function(){
+//         document.write("My height is " + this.height+ "and my rizz is" + this.rizz)
+//     };
+// };
+
+// function Bagana(rizz, height, weight, aerodynamics){
+//     this.rizz = rizz;
+//     this.height = height;
+//     this.weight = weight;
+//     this.aerodynamics = aerodynamics;
+//     return "my rizz is" + rizz + "my height is" + height + " and other things" + weight + aerodynamics
+// };
+
+// function Balghi(rizz, height, weight, aerodynamics){
+//     this.rizz = rizz;
+//     this.height = height;
+//     this.weight = weight;
+//     this.aerodynamics = aerodynamics;
+//     return "my rizz is" + rizz + "my height is" + height + " and other things" + weight + aerodynamics
+// }
+
+// let input1 = prompt("Enter the height for Bavshvi");
+// let input2 = prompt("Enter the height for Bagana");
+// let input3 = prompt("Enter the height for Balghi");
+
+// let newbavshvi = new Bavshvi("rizz value", input1, "weight value", "aerodynamics value");
+// let newbagana = new Bagana("rizz value", input2, "weight value", "aerodynamics value");
+// let newbalghi = new Balghi("rizz value", input3, "weight value", "aerodynamics value");
+
+
+// printer();
+
+
+// Constructor function
+function Bavshvi(rizz, height, weight, aerodynamics) {
     this.rizz = rizz;
     this.height = height;
     this.weight = weight;
     this.aerodynamics = aerodynamics;
-    this.printer = function(){
-        document.write("My height is " + this.height+ "and my rizz is" + this.rizz)
+    this.printer = function() {
+        document.write("I am bavshvi My height is " + this.height + " and my rizz is " + this.rizz);
     };
-};
-
-function Bagana(rizz, height, weight, aerodynamics){
-    this.rizz = rizz;
-    this.height = height;
-    this.weight = weight;
-    this.aerodynamics = aerodynamics;
-};
-
-function Balghi(rizz, height, weight, aerodynamics){
-    this.rizz = rizz;
-    this.height = height;
-    this.weight = weight;
-    this.aerodynamics = aerodynamics;
 }
 
-let input1 = prompt("Enter the height for Bavshvi");
-let input2 = prompt("Enter the height for Bagana");
-let input3 = prompt("Enter the height for Balghi");
+// Instantiate objects
+let input1 = prompt("Enter the rizz for Bavshvi");
+let input2 = prompt("Enter the rizz for Bagana");
+let input3 = prompt("Enter the rizz for Balghi");
 
-let newbavshvi = new Bavshvi("rizz value", input1, "weight value", "aerodynamics value");
-let newbagana = new Bagana("rizz value", input2, "weight value", "aerodynamics value");
-let newbalghi = new Balghi("rizz value", input3, "weight value", "aerodynamics value");
+let newbavshvi = new Bavshvi(input1, input2, "weight value", "aerodynamics value");
+// You need to replace "height value", "weight value", and "aerodynamics value" with actual values.
 
-let totalHeightLength = newbavshvi.height.length + newbagana.height.length + newbalghi.height.length;
-document.write(totalHeightLength);
+// Call the printer method
+newbavshvi.printer();
+
+
+
+// let totalHeightLength = newbavshvi.height.length + newbagana.height.length + newbalghi.height.length;
+// document.write(totalHeightLength);
 
